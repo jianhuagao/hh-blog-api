c34a2afa352bae21b5aeefe0405817438ab08310
gaojianhua <jianhua.gao@foxmail.com>
2021-01-28 16:40:06 +0800

    bug

--- config/plugin.js
+++ config/plugin.js
@@ -9,10 +9,10 @@ exports.validate = {
   enable: true,
   package: 'egg-validate',
 };
-// exports.cors = {
-//   enable: true,
-//   package: 'egg-cors',
-// };
+exports.cors = {
+  enable: true,
+  package: 'egg-cors',
+};
 exports.multer = {
   enable: true,
   package: 'koa-multer',
